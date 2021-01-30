7f06c5f3823bc006671cf91bdd1957c8d23e850a
Yuya Nagai <ynny.opem@gmail.com>
2021-01-31 08:57:33 +0900

    Modified fish.config

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ set PATH $HOME/.local/bin $PATH
 set -x SHELL /usr/bin/fish
 
 alias s sudo
+alias se sudoedit
 # direnv を使用できるようにする
 eval (direnv hook fish)
 
@@ -23,7 +24,9 @@ if [ -n is_wsl ]
     if ! pgrep gpg-agent > /dev/null;
         gpg-agent --homedir /home/yn/.gnupg --daemon > /dev/null
     end
-    set -U SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    if [ -z $SSH_AUTH_SOCK ]
+        set -U SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    end
     alias vivaldi /mnt/c/users/ynnys/AppData/Local/Vivaldi/Application/vivaldi.exe
     alias psh /mnt/c/Windows/System32/WindowsPowerShell/v1.0//powershell.exe
     # 引数として -ArgumentList '-Command ~' を与えてやれば，~ を実行してくれる
@@ -31,4 +34,5 @@ if [ -n is_wsl ]
     # 自分の公開鍵を取得する
     alias gpk "curl -s https://github.com/yn-git.keys | clip.exe" 
     alias sshfordocker "ssh -NL localhost:23750:/var/run/docker.sock docker-host &"
+    alias cmd_to_deploy_dotfiles "echo 'git clone https://github.com/ynny-github/dotfiles.git && cd dotfiles && ./install.fish' | clip.exe "
 end
