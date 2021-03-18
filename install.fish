e42dac01c68fb24eb079ae454422a85e6c14577f
Yuya Nagai <ynny.opem@gmail.com>
2021-03-18 22:52:00 +0900

    direnv の layout 機能で gatsbyjs の初期化をできるようにした

--- install.fish
+++ install.fish
@@ -3,7 +3,12 @@ if [ ! -d ~/.config ]
     mkdir -p ~/.config
 end
 
+if [ ! -d ~/.config/direnv ]
+    mkdir -p ~/.config/direnv
+end
+
 set PREFIX ~/dotfiles
 
-ln -sf $PREFIX/fish/* ~/.config/fish/
-ln -sf $PREFIX/.textlintrc.json ~/.config/.textlintrc.json
+ln -sf ~/$PREFIX/fish ~/.config/fish
+ln -sf ~/$PREFIX/.textlintrc.json ~/.config/.textlintrc.json
+ln -sf ~/$PREFIX/direnv ~/.config/direnv
