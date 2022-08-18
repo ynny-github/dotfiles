9bb1e77be1a5a3fe3955e6b63f98bff17968a7d4
Yuya Nagai <open@ynny.dev>
2022-08-18 23:03:38 +0900

    manual commit

--- dot_config/fish/functions/karabiner-elements.fish
+++
@@ -1,8 +0,0 @@
-function karabiner-elements
-    argparse -n karabiner-elements "update-apps-using-fn-keys"
-    cd ~/.config/karabiner
-
-    if set -lq _flag_update-apps-using-fn-keys
-        python3 app_function_keys_work_as_fn_keys.py
-    end
-end
