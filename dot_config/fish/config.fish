2e3056ac8fe6b4c3acc611a37fce78c4b2dd8578
Yuya Nagai <open@ynny.dev>
2022-10-06 23:59:54 +0900

    Update dot_config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -84,7 +84,8 @@ if is_wsl
     alias gpk "curl -s https://github.com/yn-git.keys | clip.exe"
     alias sshfordocker "ssh -NL localhost:23750:/var/run/docker.sock docker-host &"
     alias cmd_to_deploy_dotfiles "echo 'git clone https://github.com/ynny-github/dotfiles.git && cd dotfiles && ./install.fish' | clip.exe "end
-
+    alias cc "/mnt/c/Users/yn/scoop/apps/python/current/python.exe ~/.local/bin/custom_command.py"
+    
     cd ~
 end
 
