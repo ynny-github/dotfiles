289c799a683fbfa171a97e442452966317577b06
Yuya Nagai <open@ynny.dev>
2023-01-08 18:02:44 +0900

    Remove pyenv and iterm2

--- dot_config/fish/conf.d/main-mac.fish
+++ dot_config/fish/conf.d/main-mac.fish
@@ -3,11 +3,6 @@ if test "$CID" = "main-macbook"
     # must absolute path
     set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 
-    # Python Settings
-    set -x PYENV_ROOT $HOME/.pyenv
-    set -x PATH  $PYENV_ROOT/bin $PATH
-    pyenv init - | source
-
     fish_add_path /opt/homebrew/bin
 
     # because mac can't open code.
@@ -17,7 +12,4 @@ if test "$CID" = "main-macbook"
 
     # Open Xcode from cmd
     alias xcode="open -a /Applications/Xcode.app"
-
-    # Written by app
-    test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 end
