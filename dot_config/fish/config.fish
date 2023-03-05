7cf868bcf2aefc07b3cf56991aa53be3cfdbaec7
Yuya Nagai <open@ynny.dev>
2023-03-05 16:07:02 +0900

    Update

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -10,6 +10,11 @@ if test -f ~/.asdf/asdf.fish
     source ~/.asdf/asdf.fish
 end
 
+if command -v direnv > /dev/null
+    set -gx ASDF_DIRENV_BIN "$(asdf which direnv)"
+    $ASDF_DIRENV_BIN hook fish | source
+end
+
 if command -v starship > /dev/null
     starship init fish | source
 end
